d27c79a037724b890236507a336bff7ed33fded4
AleksandrHexlet <80881791+AleksandrAVK@users.noreply.github.com>
2022-10-27 21:17:47 +0300

    add to DEV_J110_3

---
+++ src/main/java/mainpack/LinkedList.java
@@ -0,0 +1,4 @@
+package src.main.java.mainpack;
+
+public class w {
+}
---
+++ src/main/java/mainpack/MainClass.java
@@ -0,0 +1,10 @@
+package src.main.java.mainpack;
+
+import java.util.LinkedList;
+
+public class MainClass {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList<>();
+        System.out.println("");
+    }
+}
